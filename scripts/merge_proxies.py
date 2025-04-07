0f1f339fd7b529f29f8f3df1814ac9254507836e
busymilk <1061017244@qq.com>
2025-04-07 19:09:20 +0800

    Update merge_proxies.py

--- scripts/merge_proxies.py
+++ scripts/merge_proxies.py
@@ -19,19 +19,23 @@ seen = set()
 for file in glob.glob("external_proxies/*.yaml"):
     try:
         logging.info(f"开始处理文件: {file}")
+        print(f"开始处理文件: {file}")
         with open(file, 'r',encoding="utf-8") as f:
             data = yaml.safe_load(f)
             
             if not data:
                 logging.warning(f"文件内容为空: {file}")
+                print(f"文件内容为空: {file}")
                 continue
                 
             if 'proxies' not in data:
                 logging.warning(f"文件缺少proxies字段: {file}")
+                print(f"文件缺少proxies字段: {file}")
                 continue
 
             file_proxies = data['proxies']
             logging.info(f"从文件 {file} 中发现 {len(file_proxies)} 个代理")
+            print(f"从文件 {file} 中发现 {len(file_proxies)} 个代理")
             
             if data and 'proxies' in data:
                 for proxy in data['proxies']:
@@ -41,6 +45,7 @@ for file in glob.glob("external_proxies/*.yaml"):
                         seen.add(name)
                         proxies.append(proxy)
             logging.info(f"文件处理完成: {file}")
+            print(f"文件处理完成: {file}")
     except Exception as e:
         logging.error(f"处理文件 {file} 时发生错误", exc_info=True)
         print(f"Error processing {file}: {e}")
